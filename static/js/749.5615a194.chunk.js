4e2b1c0be396979bc9c4b61da381e4fa8c89bad2
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-04-24 16:12:35 +0300

    Updates

---
+++ static/js/749.5615a194.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktz_forwork_cards=self.webpackChunktz_forwork_cards||[]).push([[749],{586:function(c,e,r){r.r(e),r.d(e,{default:function(){return a}});var s=r.p+"static/media/welcom.a930639077f5e3f0f9c1.jpg",t=r(184),a=function(){return(0,t.jsx)("div",{className:"home",children:(0,t.jsx)("img",{src:s,alt:"welcomLogo"})})}}}]);
+//# sourceMappingURL=749.5615a194.chunk.js.map
